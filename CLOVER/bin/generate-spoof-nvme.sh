8709768857f53902b92b6cfb05a8d2c7d3d558ae
Koushik Dutta <koushd@gmail.com>
2017-01-05 19:51:09 -0800

    renane

--- CLOVER/bin/generate-spoof-nvme.sh
+++ CLOVER/bin/generate-spoof-nvme.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 cd $(dirname $0)
-python find-nvme.py > ../ACPI/patched/SSDT-NVMe-Pcc.aml
+python generate-spoof-nvme.py > ../ACPI/patched/SSDT-NVMe-Pcc.aml
 ACPICA/iasl ../ACPI/patched/SSDT-NVMe-Pcc.aml
