fe52d813e0e51e54a4421d333391eafad67376c8
Koushik Dutta <koushd@gmail.com>
2020-11-17 00:16:12 -0800

    wip

--- CLOVER/bin/ACPICA/README.md
+++
@@ -1,3 +0,0 @@
-Compiled ACPI tools. Handy for compiling SSDT.aml files using the command line.
-
-https://acpica.org/downloads
--- CLOVER/bin/generate-spoof-nvme.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-cd $(dirname $0)
-python generate-spoof-nvme.py > ../ACPI/patched/SSDT-NVMe-Pcc.aml
-ACPICA/iasl ../ACPI/patched/SSDT-NVMe-Pcc.aml
